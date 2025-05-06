3adbee51f8c103707968dd7fa0dc5500565810c2
junseo0b <wnstj3968@naver.com>
2025-05-06 22:13:04 +0900

    EnergyBarWithCharacter

--- Assets/Junseo/UI Script/EnergyBarUI.cs
+++ Assets/Junseo/UI Script/EnergyBarUI.cs
@@ -10,7 +10,16 @@ public class EnergyBarUI : MonoBehaviour
 
     void Start()
     {
-        playerStatus = playerStatusProvider as PlayerStatusInterface;
+        playerStatus = playerStatusProvider.GetComponent<PlayerStatus>();
+
+        if (playerStatus == null)
+        {
+            Debug.LogError("[EnergyBarUI] PlayerStatusInterface 캐스팅 실패함!");
+        }
+        else
+        {
+            Debug.Log("[EnergyBarUI] 캐스팅 성공, 에너지: " + playerStatus.GetEnergy());
+        }
     }
 
     void Update()
@@ -19,6 +28,7 @@ public class EnergyBarUI : MonoBehaviour
         {
             float energy = playerStatus.GetEnergy();             // 에너지 값 받아오기 (예: 75)
             float ratio = Mathf.Clamp01(energy / 100f);          // 0~1 사이로 변환
+            Debug.Log($"[EnergyBarUI] energy: {energy}, ratio: {ratio}");
             barFillImage.fillAmount = ratio;                     // 이미지 바 채우기
         }
     }
