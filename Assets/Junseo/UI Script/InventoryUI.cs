bd6a0fcca2eb44e3013b42ee57203b13c492bd0d
junseo0b <wnstj3968@naver.com>
2025-05-13 19:23:48 +0900

    InventoryFix

--- Assets/Junseo/UI Script/InventoryUI.cs
+++ Assets/Junseo/UI Script/InventoryUI.cs
@@ -4,7 +4,6 @@ using UnityEngine.UI;
 public class InventoryUI : MonoBehaviour
 {
     public Image slotKey;
-    public Image slotBlanket;
     public Image slotStar1;
     public Image slotStar2;
     public Image slotStar3;
@@ -12,7 +11,6 @@ public class InventoryUI : MonoBehaviour
     void Start()
     {
         slotKey.enabled = false;
-        slotBlanket.enabled = false;
         slotStar1.enabled = false;
         slotStar2.enabled = false;
         slotStar3.enabled = false;
@@ -25,8 +23,6 @@ public class InventoryUI : MonoBehaviour
         Debug.Log("[InventoryUI] UI 갱신 호출됨");
         // 열쇠와 담요는 보유 여부만 반영
         slotKey.enabled = inventory.HasItem("Key");
-        slotBlanket.enabled = inventory.HasItem("Blanket");
-
         // 별조각 수만큼 표시
         int starCount = inventory.GetItemCount("Star");
         slotStar1.enabled = starCount >= 1;
