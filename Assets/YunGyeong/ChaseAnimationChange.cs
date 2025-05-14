c56f3f3de53e8e9539217932815217c4f9872649
yun05199 <yunn0519@naver.com>
2025-05-15 04:56:49 +0900

    애니메이션 (초안)

---
+++ Assets/YunGyeong/ChaseAnimationChange.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ChaseAnimationChange : MonoBehaviour
+{
+    private Animator animator;
+    private bool IsChase = false;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        // 아직 추격 상태가 아니고, x 좌표가 0 초과면 추격 모드로 전환
+        if (!IsChase && transform.position.x > 0f)
+        {
+            IsChase = true;
+            animator.SetBool("IsChase", true);
+            Debug.Log("[PlayerMove] 추격 애니메이션 상태 진입!");
+        }
+    }
+}
