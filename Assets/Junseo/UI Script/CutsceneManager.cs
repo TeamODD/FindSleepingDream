d14fc1dc4b7899dfa04dc8ba512160c493fc890f
junseo0b <wnstj3968@naver.com>
2025-05-14 22:46:39 +0900

    AfterCutsceneFixing

--- Assets/Junseo/UI Script/CutsceneManager.cs
+++ Assets/Junseo/UI Script/CutsceneManager.cs
@@ -89,12 +89,6 @@ public class CutsceneManager : MonoBehaviour
         {
             action.OnCutsceneEnded(index);
         }
-
-        var allResetters = FindObjectsOfType<GameOverResetManager>();
-        foreach (var resetter in allResetters)
-        {
-            resetter.OnCutsceneEnded(index);
-        }
     }
 
     private void Update()
