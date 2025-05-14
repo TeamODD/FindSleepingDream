f3fef17437e43a238a581b4c349c3e9fd3a3a5e9
HOWAREYOU1021 <kun9832@gmail.com>
2025-05-15 03:33:46 +0900

    1차 추격씬 스턴 구현
    프리팹에 스크립트 추가.
    SpawnTwo 스크립트에는 건들것 없음.
    
    변경해야할 것
    1. 플레이어 스크립트에 스턴 함수 구현
    2. 프리팹에 들어가있는 Stun 스크립트에 플레이어 스크립트 이름과 스턴함수 이름 넣기

---
+++ Assets/Script/Object/Stun.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Stun : MonoBehaviour
+{
+    
+    //public float stunTime = 1.5f;
+
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+    //    if (collision.collider.CompareTag("Player"))
+    //    {
+    //        [플레이어 스크립트 이름] player = collision.collider.GetComponent<[플레이어 스크립트 이름]>();
+    //        if (player != null)
+    //        {
+    //            player.[스턴 함수 이름](stunTime);
+    //            Debug.Log("플레이어 스턴");
+    //        }
+
+
+    //    }
+    //    if (collision.collider.CompareTag("[바닥태그]"))  <------- 바닥에 이미 있는 오브젝트와 충돌했을때 스턴먹지않게 변경
+    //    {
+    //        stunTime = 0f;
+    //        Debug.Log("스턴 초기화");
+
+    //    }
+
+    //}
+}
