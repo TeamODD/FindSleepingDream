b488efd5376802a93214322443b5b6f16226c01f
yun05199 <yunn0519@naver.com>
2025-05-15 17:18:10 +0900

    kid 스크립트 작성 및 3명 생성

---
+++ Assets/YunGyeong/KidAction.cs
@@ -0,0 +1,63 @@
+using System.Xml.Linq;
+using UnityEngine;
+
+public class KidAction : MonoBehaviour
+{
+    public Animator animator;
+    public Transform player;
+    public GameObject PrisonDoor;
+    public float checkDelay = 0.5f;
+    public bool defaultLookingRight = true;
+    public float stopDistance = 3f; //  캐릭터마다 거리 다르게!
+
+    private bool Walk = false;
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        InvokeRepeating(nameof(PrisonDoorMethod), 0f, checkDelay);
+
+        var rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.gravityScale = 0f; //  중력 꺼줌
+        }
+
+    }
+
+    public void PrisonDoorMethod()
+    {
+        if (Walk) return;
+
+        if (PrisonDoor == null)
+        {
+            Walk = true;
+            animator.SetTrigger("Walk");
+            Debug.Log($"[{name}] 탈출 시작!");
+        }
+    }
+
+    void Update()
+    {
+        if (Walk)
+        {
+            Vector3 currentPos = transform.position;
+            Vector3 targetPos = player.position;
+
+           
+            targetPos.z = currentPos.z;
+
+            float distance = Vector3.Distance(currentPos, player.position);
+            bool walking = distance > stopDistance;
+
+            animator.SetBool("Walk", walking);
+
+            if (walking)
+                transform.position = Vector3.MoveTowards(currentPos, targetPos, 2f * Time.deltaTime);
+
+            if (spriteRenderer != null)
+                spriteRenderer.flipX = (player.position.x < transform.position.x) != defaultLookingRight;
+        }
+    }
+}
